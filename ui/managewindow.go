3198e07eca0e2675aca1ce6b93d5524578385b1d
payanag2 <payanag@atomicmail.io>
2026-08-16 03:14:03 +0330

    ui: isolate standalone window class and branding

--- ui/managewindow.go
+++ ui/managewindow.go
@@ -78,14 +78,162 @@ func NewManageTunnelsWindow() (*ManageTunnelsWindow, error) {
 	mtw.SetLayout(vlayout)
 	mtw.Closing().Attach(func(canceled *bool, reason walk.CloseReason) {
 		// "Close to tray" instead of exiting application
-		if reason == walk.CloseReasonWindowClosing {
-			*canceled = true
-			mtw.SetVisible(false)
+		*canceled = true
+		if !noTrayAvailable {
+			mtw.Hide()
+		} else {
+			win.ShowWindow(mtw.Handle(), win.SW_MINIMIZE)
 		}
 	})
+	mtw.VisibleChanged().Attach(func() {
+		if mtw.Visible() {
+			mtw.tunnelsPage.updateConfView()
+			win.SetForegroundWindow(mtw.Handle())
+			win.BringWindowToTop(mtw.Handle())
+			mtw.logPage.scrollToBottom()
+		}
+	})
+
+	if mtw.tabs, err = walk.NewTabWidget(mtw); err != nil {
+		return nil, err
+	}
+
+	if mtw.tunnelsPage, err = NewTunnelsPage(); err != nil {
+		return nil, err
+	}
+	mtw.tabs.Pages().Add(mtw.tunnelsPage.TabPage)
+	mtw.tunnelsPage.CreateToolbar()
+
+	if mtw.logPage, err = NewLogPage(); err != nil {
+		return nil, err
+	}
+	mtw.tabs.Pages().Add(mtw.logPage.TabPage)
+
+	mtw.tunnelChangedCB = manager.IPCClientRegisterTunnelChange(mtw.onTunnelChange)
+	globalState, _ := manager.IPCClientGlobalState()
+	mtw.onTunnelChange(nil, manager.TunnelUnknown, globalState, nil)
+
+	systemMenu := win.GetSystemMenu(mtw.Handle(), false)
+	if systemMenu != 0 {
+		win.InsertMenuItem(systemMenu, 0, true, &win.MENUITEMINFO{
+			CbSize:     uint32(unsafe.Sizeof(win.MENUITEMINFO{})),
+			FMask:      win.MIIM_ID | win.MIIM_STRING | win.MIIM_FTYPE,
+			FType:      win.MIIM_STRING,
+			DwTypeData: windows.StringToUTF16Ptr(l18n.Sprintf("&About MyAmneziaWG…")),
+			WID:        uint32(aboutWireGuardCmd),
+		})
+		win.InsertMenuItem(systemMenu, 1, true, &win.MENUITEMINFO{
+			CbSize: uint32(unsafe.Sizeof(win.MENUITEMINFO{})),
+			FMask:  win.MIIM_TYPE,
+			FType:  win.MFT_SEPARATOR,
+		})
+	}
+
+	disposables.Spare()
 
 	return mtw, nil
 }
 
-var _ = l18n.Sprintf
-var _ = unsafe.Sizeof
+func (mtw *ManageTunnelsWindow) Dispose() {
+	if mtw.tunnelChangedCB != nil {
+		mtw.tunnelChangedCB.Unregister()
+		mtw.tunnelChangedCB = nil
+	}
+	mtw.FormBase.Dispose()
+}
+
+func (mtw *ManageTunnelsWindow) updateProgressIndicator(globalState manager.TunnelState) {
+	pi := mtw.ProgressIndicator()
+	if pi == nil {
+		return
+	}
+	switch globalState {
+	case manager.TunnelStopping, manager.TunnelStarting:
+		pi.SetState(walk.PIIndeterminate)
+	default:
+		pi.SetState(walk.PINoProgress)
+	}
+	if icon, err := iconForState(globalState, 16); err == nil {
+		if globalState == manager.TunnelStopped {
+			icon = nil
+		}
+		pi.SetOverlayIcon(icon, textForState(globalState, false))
+	}
+}
+
+func (mtw *ManageTunnelsWindow) onTunnelChange(tunnel *manager.Tunnel, state, globalState manager.TunnelState, err error) {
+	mtw.Synchronize(func() {
+		mtw.updateProgressIndicator(globalState)
+
+		if err != nil && mtw.Visible() {
+			errMsg := err.Error()
+			if len(errMsg) > 0 && errMsg[len(errMsg)-1] != '.' {
+				errMsg += "."
+			}
+			showWarningCustom(mtw, l18n.Sprintf("Tunnel Error"), l18n.Sprintf("%s\n\nPlease consult the log for more information.", errMsg))
+		}
+	})
+}
+
+func (mtw *ManageTunnelsWindow) UpdateFound() {
+	if mtw.updatePage != nil {
+		return
+	}
+	if IsAdmin {
+		mtw.SetTitle(l18n.Sprintf("%s (out of date)", mtw.Title()))
+	}
+	updatePage, err := NewUpdatePage()
+	if err == nil {
+		mtw.updatePage = updatePage
+		mtw.tabs.Pages().Add(updatePage.TabPage)
+	}
+}
+
+func (mtw *ManageTunnelsWindow) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	switch msg {
+	case win.WM_QUERYENDSESSION:
+		if lParam == win.ENDSESSION_CLOSEAPP {
+			return win.TRUE
+		}
+	case win.WM_ENDSESSION:
+		if lParam == win.ENDSESSION_CLOSEAPP && wParam == 1 {
+			walk.App().Exit(198)
+		}
+	case win.WM_SYSCOMMAND:
+		if wParam == aboutWireGuardCmd {
+			onAbout(mtw)
+			return 0
+		}
+	case raiseMsg:
+		if mtw.tunnelsPage == nil || mtw.tabs == nil {
+			mtw.Synchronize(func() {
+				mtw.SendMessage(msg, wParam, lParam)
+			})
+			return 0
+		}
+		if !mtw.Visible() {
+			mtw.tunnelsPage.listView.SelectFirstActiveTunnel()
+			if mtw.tabs.Pages().Len() != 3 {
+				mtw.tabs.SetCurrentIndex(0)
+			}
+		}
+		if mtw.tabs.Pages().Len() == 3 {
+			mtw.tabs.SetCurrentIndex(2)
+		}
+		raise(mtw.Handle())
+		return 0
+	case taskbarButtonCreatedMsg:
+		ret := mtw.FormBase.WndProc(hwnd, msg, wParam, lParam)
+		go func() {
+			globalState, err := manager.IPCClientGlobalState()
+			if err == nil {
+				mtw.Synchronize(func() {
+					mtw.updateProgressIndicator(globalState)
+				})
+			}
+		}()
+		return ret
+	}
+
+	return mtw.FormBase.WndProc(hwnd, msg, wParam, lParam)
+}
