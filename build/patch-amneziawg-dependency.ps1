d038bcb27d4ff8a45bcc45e98020c18057da6afc
payanag2 <payanag@atomicmail.io>
2026-08-16 03:39:22 +0330

    build: patch dependency service namespace for standalone client

---
+++ build/patch-amneziawg-dependency.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = 'Stop'
+
+$module = 'github.com/amnezia-vpn/amneziawg-windows/v3'
+$moduleDir = (& go list -m -f '{{.Dir}}' $module).Trim()
+if (-not $moduleDir -or -not (Test-Path $moduleDir)) {
+    throw "Unable to locate dependency module: $module"
+}
+
+$namesFile = Join-Path $moduleDir 'services\names.go'
+if (-not (Test-Path $namesFile)) {
+    throw "Dependency service names file not found: $namesFile"
+}
+
+$content = Get-Content $namesFile -Raw
+$content = $content.Replace('AmneziaWGTunnel$' + '" + tunnelName', 'MyAmneziaWGTunnel$' + '" + tunnelName')
+$content = $content.Replace('ProtectedPrefix\\Administrators\\AmneziaWG\\` + tunnelName', 'ProtectedPrefix\\Administrators\\MyAmneziaWG\\` + tunnelName')
+
+if ($content -notmatch 'MyAmneziaWGTunnel\$') {
+    throw 'Service name patch was not applied.'
+}
+if ($content -notmatch 'MyAmneziaWG\\') {
+    throw 'Named pipe path patch was not applied.'
+}
+
+Set-Content -Path $namesFile -Value $content -NoNewline
+Write-Host "Patched AmneziaWG Windows dependency: $namesFile"
